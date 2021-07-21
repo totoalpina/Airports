ff8f617a018f9965a71fbc8045439127f524bafb
totoalpina <totoalpina@gmail.com>
2021-07-21 20:54:25 +0300

    #6 ceva

--- src/main/java/ro/cosmin/airports/repository/AirportRepository.java
+++ src/main/java/ro/cosmin/airports/repository/AirportRepository.java
@@ -1,8 +1,14 @@
 package ro.cosmin.airports.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ro.cosmin.airports.entities.Airport;
+
+import java.util.List;
 
 public interface AirportRepository extends JpaRepository<Airport, Long> {
 
+//    @Query("select f from Flight f JOIN a.airport f where a.airportName = :airport and f.arrivalDate BETWEEN :start and :end")
+//    List<Airport> retrieveFlightsByArrivalAirport(@Param("airport") String airport, @Param("start") String startDate, @Param("end") String endDate);
 }
