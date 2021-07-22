f1e5b51c092f76a9ef9ff6fc889dc173a9432e72
totoalpina <totoalpina@gmail.com>
2021-07-22 14:21:14 +0300

    #10 Create functional edit html

--- src/main/java/ro/cosmin/airports/controllers/DashboardController.java
+++ src/main/java/ro/cosmin/airports/controllers/DashboardController.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import ro.cosmin.airports.models.FlightDto;
 import ro.cosmin.airports.services.AirlineService;
+import ro.cosmin.airports.services.AirportService;
 import ro.cosmin.airports.services.FlightService;
 
 
@@ -20,6 +19,9 @@ public class DashboardController {
 
     @Autowired
     private AirlineService airlineService;
+
+    @Autowired
+    private AirportService airportService;
 
     @GetMapping("/dashboard")
     @PreAuthorize("hasRole('ADMIN')")
@@ -37,12 +39,22 @@ public class DashboardController {
         return "redirect:/dashboard?success";
     }
 
-    @PostMapping("/edit/{id}")
+    @GetMapping("/editFlight/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public String editFlight(@PathVariable("flight.id") Long id, Model model) {
-        model.addAttribute("flight", flightService.findById(id));
-        flightService.updateFlight(id);
-        return "redirect:/dashboard?success";
+    public String showEditPage(@PathVariable("id") Long id, Model model) {
+        FlightDto flight = flightService.findById(id).get();
+        model.addAttribute("flight", flight);
+        model.addAttribute("editAirlines", airlineService.findAll());
+        model.addAttribute("arrivalAirports", airportService.findAll());
+        model.addAttribute("departureAirports", airportService.findAll());
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String editFlight(@ModelAttribute FlightDto flight) {
+        flightService.updateFlight(flight.getId());
+        return "redirect:/dashboard";
     }
 
 }
